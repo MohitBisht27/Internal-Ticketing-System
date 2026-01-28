2c8c8e90cb847358c88c4325ae0ac29eef8eb573
Mohit Bisht <bishtmohit2023@gmail.com>
2026-01-28 11:14:23 +0530

    Remove unused components and files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
-import SignIn from "./pages/Sigin";
-
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen">
-        <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<SignIn></SignIn>} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <h1>Working</h1>;
 }
 
 export default App;
